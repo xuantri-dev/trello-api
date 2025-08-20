20184a937dd2273070c4250bd9c33771cb788b03
TuiTenXT <tritxps28458@fpt.edu.vn>
2025-08-20 16:42:30 +0700

    Init Code base

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true, // ; ở cuối dòng
+  singleQuote: false, // dùng "" thay vì ''
+  trailingComma: "es5", // thêm dấu , ở cuối object/array (ES5)
+  tabWidth: 2, // 2 spaces
+  printWidth: 100, // wrap line
+  endOfLine: "lf", // ⭐ ép dùng LF để khỏi conflict CRLF
+};
--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
-// const express = require("express");
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+
 import express from "express";
+import { mapOrder } from "~/utils/sorts.js";
 
 const app = express();
 
@@ -7,11 +13,26 @@ const hostname = "localhost";
 const port = 8017;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world nodejs xuantridev</h1>");
+  // Test Absolute import mapOrder
+  console.log(
+    mapOrder(
+      [
+        { id: "id-1", name: "One" },
+        { id: "id-2", name: "Two" },
+        { id: "id-3", name: "Three" },
+        { id: "id-4", name: "Four" },
+        { id: "id-5", name: "Five" },
+      ],
+      ["id-5", "id-4", "id-2", "id-3", "id-1"],
+      "id"
+    )
+  );
+  res.end("<h1>Hello World!</h1><hr>");
 });
 
 app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
   console.log(
-    `Hello world nodejs xuantridev, I'm running at http://${hostname}:${port}/`
+    `Hello Xuan Tri Dev, I am running at http://${hostname}:${port}/`
   );
 });
