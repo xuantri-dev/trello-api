6fa8fc39e20c13a6d81895466f8db699bce0bbe9
TuiTenXT <tritxps28458@fpt.edu.vn>
2025-09-02 23:52:57 +0700

    express router

--- src/server.js
+++ src/server.js
@@ -8,13 +8,12 @@ import express from "express";
 import exitHook from "async-exit-hook";
 import { CONNECT_DB, CLOSE_DB } from "./config/mongodb.js";
 import { env } from "~/config/environment.js";
+import { APIs_V1 } from "~/routes/v1";
 
 const START_SERVER = () => {
   const app = express();
 
-  app.get("/", async (req, res) => {
-    res.end("<h1>Hello World!</h1><hr>");
-  });
+  app.use("/v1", APIs_V1);
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`3. Hi ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`);
