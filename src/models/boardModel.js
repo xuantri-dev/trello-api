c9d11d6f19de12e9610268e9d0fa34c77100ac0a
TuiTenXT <tritxps28458@fpt.edu.vn>
2025-09-09 12:56:06 +0700

    api create board

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -5,18 +5,49 @@
  */
 
 import Joi from "joi";
+import { GET_DB } from "~/config/mongodb";
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from "~/utils/validators";
 
 // define collection (name & schema)
-const BOARD_COLLECTION_NAME = "board";
+const BOARD_COLLECTION_NAME = "boards";
 const BOARD_COLLECTION_SCHEMA = Joi.object({
   title: Joi.string().required().min(3).max(50).trim().strict(),
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(255).trim().strict(),
 
-  columnOrderIds: Joi.array().items(Joi.string()).default([]),
+  //  các item trong mảng columnOrderIds là ObjectId nên cần thêm pattern cho chuẩn
+  columnOrderIds: Joi.array()
+    .items(Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE))
+    .default([]),
+
   createdAt: Joi.date().timestamp("javascript").default(Date.now),
   updatedAt: Joi.date().timestamp("javascript").default(null),
   _destroy: Joi.boolean().default(false),
 });
 
-export const boardModel = { BOARD_COLLECTION_NAME, BOARD_COLLECTION_SCHEMA };
+const createNew = async (data) => {
+  try {
+    const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data);
+    return createdBoard;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const findOneById = async (id) => {
+  try {
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
+      _id: id,
+    });
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const boardModel = {
+  BOARD_COLLECTION_NAME,
+  BOARD_COLLECTION_SCHEMA,
+  createNew,
+  findOneById,
+};
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -5,6 +5,7 @@
  */
 
 import { slugify } from "~/utils/formatters";
+import { boardModel } from "~/models/boardModel";
 
 const createNew = async (reqBody) => {
   try {
@@ -14,8 +15,16 @@ const createNew = async (reqBody) => {
       slug: slugify(reqBody.title),
     };
 
+    // gọi tới tầng model để xử lí lưu bản ghi newBoard vào trong database
+    const createdBoard = await boardModel.createNew(newBoard);
+    console.log(createdBoard);
+
+    // lấy bản ghi board sau khi gọi (tùy mục đích dự án mà có cần bước này hay không)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId);
+    console.log(getNewBoard);
+
     // Trả kết quả về, trong Service luôn phải có return
-    return newBoard;
+    return getNewBoard;
   } catch (error) {
     throw error;
   }
