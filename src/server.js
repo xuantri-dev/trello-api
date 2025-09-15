78a6f36a7019556f23d716dea919a0c81b2fd47e
TuiTenXT <tritxps28458@fpt.edu.vn>
2025-09-15 15:09:20 +0700

    config cors

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@
  */
 
 import express from "express";
+import cors from "cors";
+import { corsOptions } from "~/config/cors";
 import exitHook from "async-exit-hook";
 import { CONNECT_DB, CLOSE_DB } from "./config/mongodb.js";
 import { env } from "~/config/environment.js";
@@ -13,6 +15,9 @@ import { errorHandlingMiddleware } from "~/middlewares/errorHandlingMiddleware.j
 
 const START_SERVER = () => {
   const app = express();
+
+  // xử lí CORS
+  app.use(cors(corsOptions));
 
   // enable req.body json data
   app.use(express.json());
